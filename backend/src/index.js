89401a70fc1639b29ff2165bfe31770578f89976
Satyam Revgade <satyamrevgade2@gmail.com>
2025-11-09 20:47:59 +0530

    crated clusture on mongodb and connected to backend and created User model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import connectDB from './lib/db.js';
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 const app = express();
 
+const connectToDB = async () => {
+    await connectDB();
+    app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+    });
+}
 app.use('/api/auth' , authRoutes);
-app.listen(5000, () => {
-    console.log(`server is running on http://localhost:5000`);
-}   );
+
+
+connectToDB();
