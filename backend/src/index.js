015845cb4c2c11a18253baec0ea21f990881da92
letsmake <satyamrevgade2@gmail.com>
2025-11-28 08:57:18 +0530

    updated cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,11 +23,15 @@ const connectToDB = async () => {
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,  
-     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+  origin: [
+    "http://localhost:5173",
+    "https://chatbox-satyam-e4th.onrender.com"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
 app.use('/api/auth' , authRoutes);
 app.use('/api/messages' , messageRoutes);
 
